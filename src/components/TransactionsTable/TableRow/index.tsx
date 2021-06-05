e3370e779c7663725eb0921e6a1287d29df816ef
Wilson Franca <wilson.franca.92@gmail.com>
2021-06-05 16:19:03 -0700

    feat: added type props to pass it down to className.

--- src/components/TransactionsTable/TableRow/index.tsx
+++ src/components/TransactionsTable/TableRow/index.tsx
@@ -5,13 +5,14 @@ interface TableRowProps {
     amount: string;
     category: string;
     date: string;
+    type: 'income' | 'outcome';
 }
 
-export function TableRow({ title, amount, category, date }: TableRowProps) {
+export function TableRow({ title, amount, type, category, date }: TableRowProps) {
     return (
         <Container>
             <td>{title}</td>
-            <td className="income">{amount}</td>
+            <td className={type}>{amount}</td>
             <td>{category}</td>
             <td>{date}</td>
         </Container>
