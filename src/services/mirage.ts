95e5fe6f65af15a9baaacf1426f309b488ec3f5e
Wilson Franca <wilson.franca.92@gmail.com>
2021-06-05 15:09:38 -0700

    feat: pre-populated fake db with some transactions.

--- src/services/mirage.ts
+++ src/services/mirage.ts
@@ -5,6 +5,29 @@ createServer({
         transaction: Model,
     },
 
+    seeds(server) {
+        server.db.loadData({
+            transactions: [
+                {
+                    id: 1,
+                    title: 'Website Development',
+                    amount: 12000,
+                    type: 'income',
+                    category: 'Work',
+                    createdAt: new Date('2021-02-12 09:00:00')
+                },
+                {
+                    id: 1,
+                    title: 'Costco',
+                    amount: 150,
+                    type: 'outcome',
+                    category: 'Groceries',
+                    createdAt: new Date('2021-02-14 11:00:00')
+                }
+            ]
+        })
+    },
+
     routes() {
         this.namespace = 'api';
 
