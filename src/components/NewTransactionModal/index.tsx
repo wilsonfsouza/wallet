f01aca4eebc145cc53ccc2a73b0dc77485f22f67
Wilson Franca <wilson.franca.92@gmail.com>
2021-06-05 13:34:19 -0700

    feat: stored data from form fields.

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -5,7 +5,7 @@ import { Container, TransactionTypeContainer } from './styles';
 import { CloseButton } from './CloseButton';
 import { RadioBox } from './RadioBox';
 import { FiArrowDownCircle, FiArrowUpCircle } from 'react-icons/fi';
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
 
 
 interface NewTransactionModalProps {
@@ -14,7 +14,15 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+    const [title, setTitle] = useState('');
+    const [amount, setAmount] = useState(0);
+    const [category, setCategory] = useState('');
+
     const [type, setType] = useState('income');
+
+    function handleCreateNewTransaction(event: FormEvent) {
+        event.preventDefault();
+    }
 
     return (
         <Modal
@@ -25,7 +33,7 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
         >
             <CloseButton onClick={onRequestClose} />
 
-            <Container>
+            <Container onSubmit={handleCreateNewTransaction}>
                 <h2>Create a new transaction</h2>
                 <Input placeholder="Title" type="text" />
                 <Input placeholder="Amout" type="number" />
