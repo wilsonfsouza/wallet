81aa0075215e9044daf6811622ed6d9c19f4ec16
Wilson Franca <wilson.franca.92@gmail.com>
2021-06-04 21:11:29 -0700

    feat: replaced fetch with axios api GET method.

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -5,7 +5,7 @@ import { Container } from "./styles";
 export const TransactionsTable = () => {
     useEffect(() => {
         api.get('/transactions')
-            .then(data => console.log(data));
+            .then(response => console.log(response.data));
     }, []);
 
     return (
