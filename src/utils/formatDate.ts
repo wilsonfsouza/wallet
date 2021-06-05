ebcda29b934f228bc5fc5d266464aa36fa20f98b
Wilson Franca <wilson.franca.92@gmail.com>
2021-06-05 16:18:18 -0700

    fix: parsed date before format it.

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,6 +1,6 @@
 import { format } from 'date-fns';
 
 export function formatDate(date: Date): string {
-    const newDate = format(date, 'P');
-    return newDate;
+    const newDate = new Date(date);
+    return format(newDate, 'P');
 }
