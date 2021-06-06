04033dbcb3a0b3a315fd2a93eff256012c699c80
Wilson Franca <wilson.franca.92@gmail.com>
2021-06-06 15:11:08 -0700

    feat: created function to create a transaction.

--- src/contexts/transactionsContext.tsx
+++ src/contexts/transactionsContext.tsx
@@ -3,17 +3,13 @@ import { api } from '../services/api';
 import { formatAmount } from '../utils/formatAmount';
 import { formatDate } from '../utils/formatDate';
 
-type ResponseTransaction = {
+type RawTransaction = {
     id: number;
     title: string;
     amount: number;
     type: 'income' | 'outcome';
     category: string;
     createdAt: Date;
-}
-
-type ApiTransactionsResponse = {
-    transactions: ResponseTransaction[]
 }
 
 type Transaction = {
@@ -25,23 +21,26 @@ type Transaction = {
     createdAt: string;
 }
 
+type TransactionDTO = Omit<RawTransaction, 'id' | 'createdAt'>;
+
 type TransactionsContextData = {
     transactions: Transaction[];
+    createTransaction: (transaction: TransactionDTO) => Promise<void>
 }
 
 interface TransactionsProviderProps {
     children: ReactNode;
 }
 
-export const TransactionsContext = createContext({} as TransactionsContextData);
+export const TransactionsContext = createContext<TransactionsContextData>({} as TransactionsContextData);
 
 export function TransactionsProvider({ children }: TransactionsProviderProps) {
     const [transactions, setTransactions] = useState<Transaction[]>([]);
 
     useEffect(() => {
-        api.get<ApiTransactionsResponse>('/transactions')
+        api.get<{ transactions: RawTransaction[] }>('/transactions')
             .then(response => {
-                const transactions = response.data.transactions
+                const { transactions } = response.data;
 
                 const formatedTransactions = transactions.map(transaction => {
                     return {
@@ -55,8 +54,28 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
             });
     }, []);
 
+    async function createTransaction(transactionInput: TransactionDTO) {
+        const response = await api.post<{ transaction: RawTransaction }>('/transactions', {
+            ...transactionInput,
+            createdAt: new Date()
+        });
+
+        const { transaction } = response.data;
+
+        const formatedTransaction = {
+            ...transaction,
+            amount: formatAmount(transaction.amount),
+            createdAt: formatDate(transaction.createdAt)
+        };
+
+        setTransactions(transactionsAvailable => [...transactionsAvailable, formatedTransaction]);
+    }
+
     return (
-        <TransactionsContext.Provider value={{ transactions }}>
+        <TransactionsContext.Provider value={{
+            transactions,
+            createTransaction
+        }}>
             {children}
         </TransactionsContext.Provider>
     )
