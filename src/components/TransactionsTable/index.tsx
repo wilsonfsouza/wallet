7d4616fcd0958404a9b7a146ab3852ba99a3d43b
Wilson Franca <wilson.franca.92@gmail.com>
2021-06-05 17:36:34 -0700

    refactor: moved the get transactions to transactions context.

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,51 +1,10 @@
-import { useEffect, useState } from "react";
-import { api } from "../../services/api";
-import { formatAmount } from "../../utils/formatAmount";
-import { formatDate } from "../../utils/formatDate";
+import { useContext } from "react";
+import { TransactionsContext } from "../../contexts/transactionsContext";
 import { Container } from "./styles";
 import { TableRow } from "./TableRow";
 
-type ResponseTransaction = {
-    id: number;
-    title: string;
-    amount: number;
-    type: 'income' | 'outcome';
-    category: string;
-    createdAt: Date;
-}
-
-type ApiTransactionsResponse = {
-    transactions: ResponseTransaction[]
-}
-
-type Transaction = {
-    id: number;
-    title: string;
-    amount: string;
-    type: 'income' | 'outcome';
-    category: string;
-    createdAt: string;
-}
-
 export const TransactionsTable = () => {
-    const [transactions, setTransactions] = useState<Transaction[]>([]);
-
-    useEffect(() => {
-        api.get<ApiTransactionsResponse>('/transactions')
-            .then(response => {
-                const transactions = response.data.transactions
-
-                const formatedTransactions = transactions.map(transaction => {
-                    return {
-                        ...transaction,
-                        amount: formatAmount(transaction.amount),
-                        createdAt: formatDate(transaction.createdAt)
-                    }
-                });
-
-                setTransactions(formatedTransactions);
-            });
-    }, []);
+    const { transactions } = useContext(TransactionsContext);
 
     return (
         <Container>
