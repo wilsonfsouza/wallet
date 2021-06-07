cec95679083541c23a55c28913d4fc8404d10f7b
Wilson Franca <wilson.franca.92@gmail.com>
2021-06-06 19:12:49 -0700

    feat: calculated income, outcome, and total summary.

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -2,25 +2,59 @@ import { FiArrowDownCircle, FiArrowUpCircle } from 'react-icons/fi';
 import { BsWallet } from 'react-icons/bs';
 import { Card } from './Card';
 import { Container } from './styles';
+import { useContext, useMemo } from 'react';
+import { TransactionsContext } from '../../contexts/transactionsContext';
+import { formatAmount } from '../../utils/formatAmount';
 
 export const Summary = () => {
+    const { transactions } = useContext(TransactionsContext);
+
+    const { income, outcome, total } = useMemo(() => {
+        const accumulatedTransactions = transactions.reduce((acc, transaction) => {
+            switch (transaction.type) {
+                case 'income':
+                    acc.income += transaction.amount
+                    acc.total += transaction.amount
+                    break;
+                case 'outcome':
+                    acc.outcome += transaction.amount
+                    acc.total -= transaction.amount
+                    break;
+                default:
+                    break;
+            }
+
+            return acc;
+        }, {
+            total: 0,
+            income: 0,
+            outcome: 0
+        });
+
+        return {
+            total: formatAmount(accumulatedTransactions.total),
+            income: formatAmount(accumulatedTransactions.income),
+            outcome: formatAmount(accumulatedTransactions.outcome)
+        }
+    }, [transactions]);
+
     return (
         <Container>
             <Card
                 title="Income"
-                amount="$1,000.00"
+                amount={income}
                 icon={FiArrowUpCircle}
                 iconColor="#12A454"
             />
             <Card
                 title="Outcome"
-                amount="-$500.00"
+                amount={`- ${outcome}`}
                 icon={FiArrowDownCircle}
                 iconColor="#E52E4D"
             />
             <Card
                 title="Total"
-                amount="$500.00"
+                amount={total}
                 icon={BsWallet}
                 iconColor="#fff"
                 textColor="#fff"
