fc05c72c5e4ef906af1c147c3e3919215c151920
Wilson Franca <wilson.franca.92@gmail.com>
2021-06-07 19:15:09 -0700

    refactor: removed modal state and function from app component and added into a specific context.

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,22 @@
 import Modal from 'react-modal';
-
-import { useCallback, useState } from 'react';
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { GlobalStyle } from './styles/global';
 import { NewTransactionModal } from './components/NewTransactionModal';
 import './services/mirage';
 import { TransactionsProvider } from './contexts/TransactionsContext';
+import { NewTransactionsModalProvider } from './contexts/NewTransactionsModalContext';
 
 Modal.setAppElement('#root');
 
 function App() {
-  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
-
-  const handleCloseNewTransactionModal = useCallback(() => {
-    setIsNewTransactionModalOpen(false)
-  }, []);
-
-  const handleOpenNewTransactionModal = useCallback(() => {
-    setIsNewTransactionModalOpen(true)
-  }, []);
-
   return (
     <TransactionsProvider>
-      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+      <NewTransactionsModalProvider>
+        <Header />
+        <NewTransactionModal />
+      </NewTransactionsModalProvider>
       <Dashboard />
-      <NewTransactionModal
-        isOpen={isNewTransactionModalOpen}
-        onRequestClose={handleCloseNewTransactionModal}
-      />
       <GlobalStyle />
     </TransactionsProvider>
   );
