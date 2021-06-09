3afb6f0cff5e70f4d980230f76e782465b176228
Wilson Franca <wilson.franca.92@gmail.com>
2021-06-08 21:28:49 -0700

    feat: selected edit and delete function from context and returned in useTransactions hook.

--- src/hooks/useTransactions.tsx
+++ src/hooks/useTransactions.tsx
@@ -4,9 +4,13 @@ import { useContextSelector } from 'use-context-selector';
 export function useTransactions() {
     const transactions = useContextSelector(TransactionsContext, transactions => transactions.transactions);
     const onCreateTransaction = useContextSelector(TransactionsContext, transactions => transactions.createTransaction);
+    const onEditTransaction = useContextSelector(TransactionsContext, transactions => transactions.editTransaction);
+    const onDeleteTransaction = useContextSelector(TransactionsContext, transactions => transactions.deleteTransaction);
 
     return {
         transactions,
-        onCreateTransaction
+        onCreateTransaction,
+        onEditTransaction,
+        onDeleteTransaction
     };
 }
