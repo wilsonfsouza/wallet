ee5020ab0bc4bd93e2e4cb6bb63f281de695cd96
Wilson Franca <wilson.franca.92@gmail.com>
2021-06-10 19:30:49 -0700

    refactor: received transaction object as props instead of each attribute separately. Also, implemented delete function.

--- src/components/TransactionsTable/TableRow/index.tsx
+++ src/components/TransactionsTable/TableRow/index.tsx
@@ -1,25 +1,38 @@
 import { RiPencilLine, RiDeleteBin2Fill } from 'react-icons/ri';
+import { useTransactions } from '../../../hooks/useTransactions';
+import { formatAmount } from '../../../utils/formatAmount';
 import { Container, ActionButton, ActionButtonsContainer } from './styles';
 
-interface TableRowProps {
+interface Transaction2 {
+    id: number;
     title: string;
-    amount: string;
+    amount: number;
     category: string;
-    date: string;
+    createdAt: string;
     type: 'income' | 'outcome';
 }
 
-export function TableRow({ title, amount, type, category, date }: TableRowProps) {
+interface TableRowProps {
+    transaction: Transaction2;
+}
+
+export function TableRow({ transaction }: TableRowProps) {
+    const { onDeleteTransaction } = useTransactions();
+
+    const { id, title, type, category, createdAt, amount } = transaction;
+
+    const formatedAmount = formatAmount(amount);
+
     return (
         <Container>
             <td>{title}</td>
-            <td className={type}>{amount}</td>
+            <td className={type}>{formatedAmount}</td>
             <td>{category}</td>
-            <td>{date}</td>
+            <td>{createdAt}</td>
             <td>
                 <ActionButtonsContainer>
                     <ActionButton colorScheme="var(--light-purple)" rightIcon={RiPencilLine}>Edit</ActionButton>
-                    <ActionButton colorScheme="var(--red)" rightIcon={RiDeleteBin2Fill}>Delete</ActionButton>
+                    <ActionButton colorScheme="var(--red)" rightIcon={RiDeleteBin2Fill} onClick={() => onDeleteTransaction(id)}>Delete</ActionButton>
                 </ActionButtonsContainer>
             </td>
         </Container>
