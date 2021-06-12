62cd4b8d747e9dc3b7c8b4c112e625c1612130c2
Wilson Franca <wilson.franca.92@gmail.com>
2021-06-11 21:58:20 -0700

    feat: implemented delete transaction logic and modal.

--- src/components/TransactionsTable/TableRow/index.tsx
+++ src/components/TransactionsTable/TableRow/index.tsx
@@ -1,7 +1,7 @@
 import { useCallback, useState } from 'react';
 import { RiPencilLine, RiDeleteBin2Fill } from 'react-icons/ri';
-import { useTransactions } from '../../../hooks/useTransactions';
 import { formatAmount } from '../../../utils/formatAmount';
+import { DeleteTransactionModal } from '../../Modals/DeleteTransaction';
 import { EditTransactionModal } from '../../Modals/EditTransaction';
 import { Container, ActionButton, ActionButtonsContainer } from './styles';
 
@@ -14,29 +14,42 @@ interface Transaction {
     type: 'income' | 'outcome';
 }
 
+type TransactionToBeDeleted = Pick<Transaction, "id" | "title">;
+
 interface TableRowProps {
     transaction: Transaction;
 }
 
 export function TableRow({ transaction }: TableRowProps) {
-    const { onDeleteTransaction } = useTransactions();
-
     const { id, title, type, category, createdAt, amount } = transaction;
 
     const formatedAmount = formatAmount(amount);
 
     const [isOpenEditTransactionModal, setIsOpenEditTransactionModal] = useState(false);
+    const [editTransaction, setEditTransaction] = useState<Transaction | null>(null);
 
     const onCloseEditTransactionModal = useCallback(() => {
         setIsOpenEditTransactionModal(false);
     }, []);
 
-    const [editTransaction, setEditTransaction] = useState<Transaction | null>(null);
 
     function handleEditTransaction(transaction: Transaction) {
         setEditTransaction(transaction);
         setIsOpenEditTransactionModal(true);
     }
+
+    const [isOpenDeleteTransactionModal, setIsOpenDeleteTransactionModal] = useState(false);
+    const [deleteTransaction, setDeleteTransaction] = useState<TransactionToBeDeleted | null>(null);
+
+    function handleDeleteTransaction(transaction: TransactionToBeDeleted) {
+        setDeleteTransaction(transaction);
+        setIsOpenDeleteTransactionModal(true);
+    }
+
+    const onCloseDeleteTransactionModal = useCallback(() => {
+        setIsOpenDeleteTransactionModal(false);
+    }, []);
+
 
     return (
         <Container>
@@ -47,11 +60,14 @@ export function TableRow({ transaction }: TableRowProps) {
             <td>
                 <ActionButtonsContainer>
                     <ActionButton colorScheme="var(--light-purple)" rightIcon={RiPencilLine} onClick={() => handleEditTransaction(transaction)}>Edit</ActionButton>
-                    <ActionButton colorScheme="var(--red)" rightIcon={RiDeleteBin2Fill} onClick={() => onDeleteTransaction(id)}>Delete</ActionButton>
+                    <ActionButton colorScheme="var(--red)" rightIcon={RiDeleteBin2Fill} onClick={() => handleDeleteTransaction({ id, title })}>Delete</ActionButton>
                 </ActionButtonsContainer>
             </td>
             {editTransaction && (
                 <EditTransactionModal isOpen={isOpenEditTransactionModal} onClose={onCloseEditTransactionModal} transaction={editTransaction} />
+            )}
+            {deleteTransaction && (
+                <DeleteTransactionModal isOpen={isOpenDeleteTransactionModal} onClose={onCloseDeleteTransactionModal} transaction={deleteTransaction} />
             )}
         </Container>
     );
